2e0f4851f3f1861628416631d18cf6ed8ec411cd
Laura <laura9michael@gmail.com>
2020-10-26 22:22:09 -0400

    Populate Table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,32 @@
 // from data.js
 var tableData = data;
-
+var tbody = d3.select("tbody");
 // YOUR CODE HERE!
+// // Step 1: Loop Through `data` and console.log each UFO Sighting object
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+// // Step 2:  Use d3 to append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+// // Step 3:  Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+// // Step 4: Use d3 to append 1 cell per UFO Sighting value 
+// // (date/time, city, state, country, shape, duration, comment)
+        var cell = row.append("td");
+// // Step 5: Use d3 to update each cell's text with UFO Sighting values
+// // (date/time, city, state, country, shape, duration, comment) 
+        cell.text(value);
+    });
+});
+
+// Refactor to use Arrow Functions!
+// data.forEach((ufoSighting) => {
+//   var row = tbody.append("tr");
+//   Object.entries(ufoSighting).forEach(([key, value]) => {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
+
+
